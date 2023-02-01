8ce32e41c1c258de0c0be2dfb650494cf99921ae
Alec Iberg <ibergalec@gmail.com>
2023-01-31 17:32:42 -0800

    writing to postgres database works

--- backend/index.go
+++ backend/index.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,76 +12,92 @@ import (
 	_ "github.com/lib/pq"
 )
 
-// yeah i will need to switch this
 type State struct {
-	name   		string `json:"name"`
-	law 		string `json:"law"`
-	lawcode 	string `json:"lawcode"`
-	date 		string `json:"date"`
-	experttake 	string `json:"expertake"`
+	id         int    `json:"stateID"`
+	name       string `json:"name"`
+	length     int    `json:"length"`
+	law        string `json:"law"`
+	lawcode    string `json:"lawcode"`
+	date       string `json:"date"`
+	experttake string `json:"expertake"`
 }
 
 // this will be useful as is
 type JsonResponse struct {
 	Type    string  `json:"type"`
-	Data    []State `json:"data"` 
+	Data    []State `json:"data"`
 	Message string  `json:"message"`
 }
+
+// TO BE AWARE OF
+// GOLANG DOES NOT HOT RELOAD
+// YOU MUST RESTART SERVER TO SEE CHANGES
 
 // Main function
 func main() {
 	// Init the mux router
 	router := mux.NewRouter()
 
-	// Route handles & endpoints
+	router.HandleFunc("/", HomeHandler)
 
-	// Get all movies
-	// router.HandleFunc("/movies/", GetMovies).Methods("GET")
+	// Get all states
+	router.HandleFunc("/states", GetStates).Methods("GET")
 
-	// need to build to get specific state
+	// to handle post requests
+	router.HandleFunc("/create", CreateState).Methods("POST")
 
 	// serve the app
-	fmt.Println("Server at 8080")
+	fmt.Println("Server at 8000")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
 const (
 	DB_USER     = "postgres"
-	DB_PASSWORD = "12345678"
-	DB_NAME     = "movies"
+	DB_PASSWORD = "password"
+	DB_NAME     = "StateInfoNonCompete"
+	// I THINK THE PASSWORD IS password
 )
 
-func GetMovies(w http.ResponseWriter, r *http.Request) {
-    db := setupDB()
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	printMessage("Hitting Home Endpoint")
+	json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+}
 
-    printMessage("Getting movies...")
+func GetStates(w http.ResponseWriter, r *http.Request) {
+	db := setupDB()
 
-    // Get all movies from movies table that don't have movieID = "1"
-    rows, err := db.Query("SELECT * FROM movies")
+	printMessage("Getting states...")
 
-    // check errors
-    checkErr(err)
+	// Get all movies from movies table that don't have movieID = "1"
+	rows, err := db.Query("SELECT * FROM states")
 
-    // var response []JsonResponse
-    var movies []Movie
+	// check errors
+	checkErr(err)
 
-    // Foreach movie
-    for rows.Next() {
-        var id int
-        var movieID string
-        var movieName string
+	// var response []JsonResponse
+	var states []State
 
-        err = rows.Scan(&id, &movieID, &movieName)
+	// Foreach movie
+	for rows.Next() {
+		var id int
+		var name string
+		var length int
+		var law string
+		var lawcode string
+		var date string
+		var experttake string
 
-        // check errors
-        checkErr(err)
+		err = rows.Scan(&id, &name, &length, &law, &lawcode, &date, &experttake)
 
-        movies = append(movies, Movie{MovieID: movieID, MovieName: movieName})
-    }
+		// check errors
+		checkErr(err)
 
-    var response = JsonResponse{Type: "success", Data: movies}
+		states = append(states, State{name: name, id: id, length: length, law: law, lawcode: lawcode, date: date, experttake: experttake})
+	}
 
-    json.NewEncoder(w).Encode(response)
+	var response = JsonResponse{Type: "success", Data: states}
+
+	json.NewEncoder(w).Encode(response)
 }
 
 // DB set up
@@ -95,12 +112,51 @@ func setupDB() *sql.DB {
 	return db
 }
 
-func checkErr("something is wrong" err error) {
-	printMessage(err)
+func CreateState(w http.ResponseWriter, r *http.Request) {
+	id := r.FormValue("id")
+	name := r.FormValue("name")
+	length := r.FormValue("length")
+	law := r.FormValue("law")
+	lawcode := r.FormValue("lawcode")
+	date := r.FormValue("date")
+	experttake := r.FormValue("experttake")
+
+	var response = JsonResponse{}
+
+	if id == "" || name == "" {
+		response = JsonResponse{Type: "error", Message: "You are missing a parameter."}
+	} else {
+		db := setupDB()
+
+		printMessage("Inserting state into DB")
+
+		fmt.Println("Inserting new movie with ID: " + id + " and name: " + name)
+
+		var lastInsertID int
+		err := db.QueryRow("INSERT INTO states(id, name, length, law, lawcode, date, experttake) VALUES($1, $2, $3, $4, $5, $6, $7) returning id;", id, name, length, law, lawcode, date, experttake).Scan(&lastInsertID)
+
+		// check errors
+		checkErr(err)
+
+		response = JsonResponse{Type: "success", Message: "The state has been inserted successfully!"}
+	}
+
+	json.NewEncoder(w).Encode(response)
 }
 
-func printMessage(message string, err error) {
-    fmt.Println("")
-    fmt.Println(message, err)
-    fmt.Println("")
+func UpdateState(w http.ResponseWriter, r *http.Request) {
+	// to update a state in the database
+}
+
+// Function for handling errors
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func printMessage(message string) {
+	fmt.Println("")
+	fmt.Println(message)
+	fmt.Println("")
 }
